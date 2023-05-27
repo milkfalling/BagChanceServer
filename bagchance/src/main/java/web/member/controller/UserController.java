8ce8f2693d2271f836f085d3d972bd5b1bd6a44a
Henry <hen85618@gmail.com>
2023-05-27 11:51:07 +0800

    測試代換關鍵字前的存檔

--- bagchance/src/main/java/web/member/controller/UserController.java
+++ bagchance/src/main/java/web/member/controller/UserController.java
@@ -4,16 +4,19 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import web.member.bean.User;
 import web.member.service.UserService;
 import web.member.service.impl.UserServiceImpl;
 
+@WebServlet("/user/*")
 public class UserController extends HttpServlet {
 	private static final Gson GSON = new Gson();
 	private static final long serialVersionUID = 1L;
@@ -22,6 +25,16 @@ public class UserController extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		service = new UserServiceImpl();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = GSON.fromJson(req.getReader(), User.class);
+		boolean result = service.register(user);
+		JsonObject respBody = new JsonObject();
+		respBody.addProperty("successful", result);
+		respBody.addProperty("message", result ? "註冊成功" : "註冊失敗");
+		resp.getWriter().write(respBody.toString());
 	}
 	
 	@Override
